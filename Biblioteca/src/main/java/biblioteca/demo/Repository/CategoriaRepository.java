b604fa47b6a36011ca357f3a24555264e7cb3883
Royv1222 <rmv2098@gmail.com>
2025-10-23 20:23:51 -0600

    Remove folders Biblioteca, SCRIPT, rvillafuerte-tienda

--- Biblioteca/src/main/java/biblioteca/demo/Repository/CategoriaRepository.java
+++
@@ -1,6 +0,0 @@
-package biblioteca.demo.repository;
-
-import biblioteca.demo.domain.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> { }
--- Biblioteca/src/main/java/biblioteca/demo/Service/QuejaService.java
+++
@@ -1,7 +0,0 @@
-package biblioteca.demo.Service;
-
-import biblioteca.demo.domain.Queja;
-
-public interface QuejaService {
-    Queja guardar(Queja q);
-}
